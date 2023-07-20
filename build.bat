9e7a7dbe9a6e6a883d585090cc51d345db3f279e
Bronco Oostermeyer <dev@bfv.io>
2023-07-20 08:00:43 +0200

    removed superfluous script

--- build.bat
+++
@@ -1,4 +0,0 @@
-docker build -t openedge-compiler:12.7 -t devbfvio/openedge-compiler:12.7.0 -t devbfvio/openedge-compiler:latest -t openedge-compiler:latest .
-rem remove intermediate containers
-docker image prune -f
-
